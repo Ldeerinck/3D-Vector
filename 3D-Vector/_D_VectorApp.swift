89b73bda36bf64ba14ab126bcab7c690a2cbb205
Luke Deerinck <ldeerinck2@me.com>
2025-11-11 12:47:32 -0800

    Initial Commit

---
+++ 3D-Vector/_D_VectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_VectorApp.swift
+//  3D-Vector
+//
+//  Created by Luke Deerinck on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _D_VectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3D-VectorTests/_D_VectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  _D_VectorTests.swift
+//  3D-VectorTests
+//
+//  Created by Luke Deerinck on 11/11/25.
+//
+
+import Testing
+@testable import _D_Vector
+
+struct _D_VectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
