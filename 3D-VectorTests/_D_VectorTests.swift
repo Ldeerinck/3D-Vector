2be505b8248857d15f5f66c12f845a8c7fdbd1bc
Luke Deerinck <ldeerinck2@me.com>
2025-11-12 11:59:57 -0800

    added some tests and introduced a tolerance value

--- 3D-VectorTests/_D_VectorTests.swift
+++ 3D-VectorTests/_D_VectorTests.swift
@@ -6,12 +6,28 @@
 //
 
 import Testing
+import Spatial
 @testable import _D_Vector
 
 struct _D_VectorTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test func colinearTest() async throws {
+        let segment1 = LineSegment(start: Point3D(x: 0, y: 0, z: 0), end: Point3D(x: 1, y: 1, z: 1))
+        let segment2 = LineSegment(start: Point3D(x: 2, y: 2, z: 2), end: Point3D(x: 3, y: 3, z: 3))
+        #expect(colinearCheck(segment1, segment2))
+        let segment3 = LineSegment(start: Point3D(x: 0, y: 0, z: 0), end: Point3D(x: 1, y: 0, z: 0))
+        let segment4 = LineSegment(start: Point3D(x: 0, y: 1, z: 0), end: Point3D(x: 0, y: 1, z: 1))
+        #expect(!colinearCheck(segment3, segment4))
+        let segment5 = LineSegment(start: Point3D(x: -3, y: -3, z: -3), end: Point3D(x: -2, y: -2, z: -2))
+        #expect(colinearCheck(segment1, segment5))
+        //P1=(1.5,2,3.5)cap P sub 1 equals open paren 1.5 comma 2 comma 3.5 close paren𝑃1=(1.5,2,3.5), \(P_{2}=(2.5,3,4.5)\), \(P_{3}=(3.5,4,5.5)\), and \(P_{4}=(4.5,5,6.5)\).
+        let segment6 = LineSegment(start: Point3D(x: 1.5, y: 2, z: 3.5), end: Point3D(x: 2.5, y: 3, z: 4.5))
+        let segment7 = LineSegment(start: Point3D(x: 3.5,y: 4,z: 5.5), end: Point3D(x: 4.5,y: 5,z: 6.5))
+        #expect(colinearCheck(segment6, segment7))
     }
 
 }
--- ColinearCheck.swift
+++ ColinearCheck.swift
@@ -8,16 +8,18 @@
 import Foundation
 import Spatial
 
-func colinearCheck(_ linesegment1: LineSegment, _ linesegment2: LineSegment) -> Bool {
+func colinearCheck(_ linesegment1: LineSegment, _ linesegment2: LineSegment, tolerance: Double = 1e-9) -> Bool {
     
     //check that the Two direction Vectors for the line segments are parallel by checking the crossproduct
     let directionVector1: Vector3D = Vector3D(x: linesegment1.end.x - linesegment1.start.x, y: linesegment1.end.y - linesegment1.start.y, z: linesegment1.end.z - linesegment1.start.z)
     let directionVector2: Vector3D = Vector3D(x: linesegment2.end.x-linesegment2.start.x,y:linesegment2.end.y-linesegment2.start.y,z:linesegment2.end.z-linesegment2.start.z)
     
-    if directionVector1.cross(directionVector2) == .zero {
-        //check that they are on the same line by checking the cross produc of segment composed of lines from both segments
-        let vector3: Vector3D = Vector3D(x: linesegment2.start.x-linesegment1.start.x,y:linesegment2.start.y-linesegment1.start.y,z:linesegment2.start.z-linesegment1.start.z)
-        if vector3.cross(directionVector1) == .zero {
+    let crossProduct1 = directionVector1.cross(directionVector2)
+    if crossProduct1.length < tolerance {
+        //check that they are on the same line by checking the cross product of a direction vector for segment composed of points from both segments against any of the direction vectors for the original line segments
+        let directionVector3: Vector3D = Vector3D(x: linesegment2.start.x-linesegment1.start.x,y:linesegment2.start.y-linesegment1.start.y,z:linesegment2.start.z-linesegment1.start.z)
+        let crossProduct2 = directionVector3.cross(directionVector1)
+        if crossProduct2.length < tolerance {
             return true
         }
         return false
